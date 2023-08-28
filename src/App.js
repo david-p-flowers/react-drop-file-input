a07adf9387f4e8c536549106c229e9f75fd98656
David <dpflower@ncsu.edu>
2023-08-28 14:55:41 -0400

    Fixed metadata bug

--- src/App.js
+++ src/App.js
@@ -1,42 +1,24 @@
 import './App.css';
-import DropFileInput from './components/drop-file-input/DropFileInput';
-import UploadButton from './components/upload-button/UploadButton';
-import VideoList from './VideoList';
 import { useState } from 'react';
 import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage"
 import { storage, db } from './firebase';
 import { collection, addDoc } from "firebase/firestore"
+import DropFileInput from './components/drop-file-input/DropFileInput';
 
 function App() {
     const [file, setFile] = useState(null);
-    const [assetname, setAssetname] = useState(""); // Initialize with an empty string
+    const [assetname, setAssetname] = useState("");
     const [gametitle, setGametitle] = useState("");
     const [description, setDescription] = useState("");
 
     const onFileChange = (files) => {
-        const currentFile = files[0]
-        setFile(currentFile)
+        const currentFile = files[0];
+        setFile(currentFile);
         console.log(files);
     }
 
-    const onAssetnameChange = (e) => {
-        const assetname = e.target.value;
-        console.log("New Assetname: ", assetname )
-        setAssetname(assetname); // Correct: Using functional update
-    }
-
-    const onGametitleChange = (e) => {
-        const newGametitle = e.target.value;
-        setGametitle(newGametitle); // Correct: Using functional update
-    }
-
-    const onDescriptionChange = (e) => {
-        const newDescription = e.target.value;
-        setDescription(newDescription); // Correct: Using functional update
-    }
-
-    const uploadToDatabase = (url, assetname, gametitle, description) => { 
-        let docData = {
+    const uploadToDatabase = (url) => { 
+        const docData = {
             mostRecentUploadURL: url,
             asset: assetname, 
             game: gametitle,
@@ -45,9 +27,8 @@ function App() {
             metadata: "test",
         }
 
-        const collectionRef = collection(db, "assets"); // Reference to the "assets" collection
+        const collectionRef = collection(db, "assets");
 
-        // Add a new document with auto-generated ID
         addDoc(collectionRef, docData)
           .then((docRef) => {
             console.log("Document added with ID: ", docRef.id);
@@ -58,7 +39,6 @@ function App() {
           .catch((error) => {
             console.error("Error adding document: ", error);
           });
-        
     }
 
     const handleClick = () => {
@@ -67,17 +47,17 @@ function App() {
         const uploadTask = uploadBytesResumable(fileRef, file)
 
         uploadTask.on('state_changed', (snapshot) => {
-            let progress = (snapshot.bytesTransferred/ snapshot.totalBytes) * 100
-            console.log(progress)
+            const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+            console.log(progress);
         }, (error) => {
-            console.log("error :", error)
+            console.log("error:", error);
         }, () => {
-            console.log("success!!")
+            console.log("success!!");
             getDownloadURL(uploadTask.snapshot.ref).then(downloadURL =>{
-                uploadToDatabase(downloadURL, assetname, gametitle, description)
-                console.log(downloadURL)
-            })
-        })
+                uploadToDatabase(downloadURL);
+                console.log(downloadURL);
+            });
+        });
     }
 
     return (
@@ -85,15 +65,39 @@ function App() {
             <h2 className="header">
                 Drop Game Assets here
             </h2>
-            <DropFileInput
-             onFileChange={(files) => onFileChange(files)}
-             onAssetnameChange={onAssetnameChange} // Check if this is being passed correctly
-             onGametitleChange={onGametitleChange} // Check if this is being passed correctly
-             onDescriptionChange={onDescriptionChange} // Check if this is being passed correctly
-            />
-            <br></br>
-            <UploadButton onClick={() => handleClick()} />
-            <VideoList />
+            <DropFileInput onFileChange={onFileChange} />
+            <br />
+            <div>
+                <p>Asset Name</p>
+                <input
+                    type="text"
+                    value={assetname}
+                    placeholder="Enter assetname"
+                    className="metadata_field"
+                    onChange={(e) => setAssetname(e.target.value)}
+                />
+
+                <p>Game Title</p>
+                <input
+                    type="text"
+                    value={gametitle}
+                    placeholder="Enter the game's title"
+                    className="metadata_field"
+                    onChange={(e) => setGametitle(e.target.value)}
+                />
+
+                <p>Description</p>
+                <input
+                    type="text"
+                    value={description}
+                    placeholder="Add a description"
+                    className="metadata_field"
+                    onChange={(e) => setDescription(e.target.value)}
+                />
+            </div>
+            <br />
+            <br />
+            <button onClick={handleClick}>Upload and Save</button>
         </div>
     );
 }
